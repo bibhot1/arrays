6b9830dd4d9ca25aaa4d77ad5049d4a1bf724969
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-18 09:05:27 +1200

    Add project files.

---
+++ arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿//arrays
+/*
+int[] numbersPractice = new int[7];
+
+numbersPractice[0] = 12;
+numbersPractice[1]= 13;
+numbersPractice[2]= 14;
+numbersPractice[3]= 15;
+numbersPractice[4]= 16;
+numbersPractice[5]= 17;
+numbersPractice[6]= 18;
+
+Console.WriteLine(numbersPractice[4]);
+*/
+
+//Directly assigning values to an array in one line
+
+// we can do it for integers
+
+int[] numberList = [25, 50, 18, 14, 25];
+Console.WriteLine(numberList[3]);
+
+// can do it for doubles as well
+
+double[] newDouble = [12.21, 14.25, 56.25];
+Console.WriteLine(newDouble[1]);
+
+// can do it for strings as well lets go
+
+string[] weekDays = ["sunday", "monday", "tuesday", " wednesday", "thursday", " friday", "saturday"];
+Console.WriteLine(weekDays[2]);
